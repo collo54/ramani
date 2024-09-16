00ff6ed1598ea82967e265cd59b2ffcb2ad3d2b3
mutugicollins54 <mutugicollins54@gmail.com>
2024-09-16 12:31:09 +0000

    feature: adds revenue cat purchases sdk and billing permisssions

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ramanirides/custom/home_scaffold.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 import 'custom/auth_state.dart';
 import 'firebase_options.dart';
@@ -17,6 +20,39 @@ void main() async {
       child: MyApp(),
     ),
   );
+
+  await _configureSDK();
+}
+
+String get androidConfigurationKey =>
+    const String.fromEnvironment('REVCAT_ANDROID_API_KEY');
+
+String get iosConfigurationKey =>
+    const String.fromEnvironment('REVCAT_IOS_API_KEY');
+
+String get webConfigurationKey =>
+    const String.fromEnvironment('REVCAT_WEB_API_KEY');
+
+Future<void> _configureSDK() async {
+  // Enable debug logs before calling `configure`.
+  await Purchases.setLogLevel(LogLevel.debug);
+
+  var webPurchaseConfig = PurchasesConfiguration(webConfigurationKey);
+  var androidPurchaseConfig = PurchasesConfiguration(androidConfigurationKey);
+  var iosPurchaseConfig = PurchasesConfiguration(iosConfigurationKey);
+
+  if (kIsWeb) {
+    return await Purchases.configure(webPurchaseConfig);
+  }
+  switch (defaultTargetPlatform) {
+    case TargetPlatform.android:
+      return await Purchases.configure(androidPurchaseConfig);
+    case TargetPlatform.iOS:
+      return await Purchases.configure(iosPurchaseConfig);
+
+    default:
+      return await Purchases.configure(androidPurchaseConfig);
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -25,7 +61,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ramani rides',
+      title: 'RamaniRide',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
